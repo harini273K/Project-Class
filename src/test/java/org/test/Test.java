bbcfa7a56be97e96872fbc5fe6acb9ef0cc22188
harini273K <haripriyahp0322@gmail.com>
2025-04-22 20:42:45 +0530

    Hari's Code1

--- src/test/java/org/test/Test.java
+++ src/test/java/org/test/Test.java
@@ -4,7 +4,10 @@ public class Test {
 	
 	public static void main(String[] args) {
 	
-		System.out.println("Test");
+		System.out.println("Test1");
+		System.out.println("Test5");
+		System.out.println("Test6");
+		System.out.println("Test7");
 		
 	}
 	
